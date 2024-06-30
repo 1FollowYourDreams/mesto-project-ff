3f17d4476d882e2e2a07bf3d032fe823e52ce498
Natalia Pavlova <missis.happy@yandex.ru>
2024-07-01 01:54:09 +0300

    mesto-project-ff

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Мальдивы",
+      link: "https://images.unsplash.com/photo-1578922746465-3a80a228f223?q=80&w=1527&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Доминикана",
+      link: "https://images.unsplash.com/photo-1595603998379-aa5736ed8b9d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Куба",
+      link: "https://images.unsplash.com/photo-1583691837385-84f8b6ca9ebf?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Крым",
+      link: "https://images.unsplash.com/photo-1711707258147-057185689e0a?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Сочи",
+      link: "https://images.unsplash.com/photo-1656579828487-acb3c1c065a7?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Батуми",
+      link: "https://images.unsplash.com/photo-1625582175018-8fc4be59ac4f?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+function createCard(data, delCard) {
+  const cardData = cardTemplate.querySelector('.card').cloneNode(true); 
+  const cardDelButton = cardData.querySelector('.card__delete-button');
+  cardData.querySelector('.card__title').textContent = data.name;
+  cardData.querySelector('.card__image').src = data.link;
+  cardData.querySelector('.card__image').alt = data.name;
 
+  cardDelButton.addEventListener('click', function () {
+    delCard(cardDelButton);
+  });
+
+  return cardData;
+}
 // @todo: Функция удаления карточки
-
+function delCard(cardDelButton) {
+    const CardDelete = cardDelButton.closest('.card');
+    CardDelete.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(data) {
+placesList.append(createCard(data, delCard))
+});
